874869b1121dcdf013b5d73dc49b8d9aca1d1939
CristopherLuisCruz <133653978+CristopherLuisCruz@users.noreply.github.com>
2023-05-18 12:10:46 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -10,20 +10,20 @@ export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'index.html');
-        about: resolve(pages, 'About.html');
-        contact: resolve(pages, 'Contact.html');
-        terms: resolve(pages, 'TOS.html');
-        copyrights: resolve(pages, 'copyright.html');
-        forest_img: resolve(images, 'Forest.jpg');
-        mountain_img: resolve(images, 'Mountain.png');
-        autumn_img: resolve(images, 'Autumn.png');
-        adventure_img: resolve(images, 'AnimeScenery.png');
-        aurora_img: resolve(images, 'Aurora.png');
-        mntrng_img: resolve(images, 'MountainRange.png');
-        river_img: resolve(images, 'River.png');
-        valley_img: resolve(images, 'ValleyForest.png');
-        side_img: resolve(images, 'Countryside.png');
+        main: resolve(__dirname, 'index.html'),
+        about: resolve(pages, 'About.html'),
+        contact: resolve(pages, 'Contact.html'),
+        terms: resolve(pages, 'TOS.html'),
+        copyrights: resolve(pages, 'copyright.html'),
+        forest_img: resolve(images, 'Forest.jpg'),
+        mountain_img: resolve(images, 'Mountain.png'),
+        autumn_img: resolve(images, 'Autumn.png'),
+        adventure_img: resolve(images, 'AnimeScenery.png'),
+        aurora_img: resolve(images, 'Aurora.png'),
+        mntrng_img: resolve(images, 'MountainRange.png'),
+        river_img: resolve(images, 'River.png'),
+        valley_img: resolve(images, 'ValleyForest.png'),
+        side_img: resolve(images, 'Countryside.png'),
       },
     },
   },
